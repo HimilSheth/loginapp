b41130c473f2d117b63eccffb3e9c940c764c339
HimilSheth <himilsheth07@example.com>
2015-06-16 17:33:21 +0530

    Sign up form done with user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
    # byebug
-
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+
+    else
+      render 'new'
+    end
+  end
+    #private
+    #  def user_params
+     #   byebug
+     #   params.require(:user).permit(:name,:email,:password,:password_confirmation)
+
+     # end
+
 end
