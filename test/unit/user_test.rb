e72168b4c6ce8c55c2d3db5bb02574f486389e98
HimilSheth <himilsheth07@example.com>
2015-06-16 13:54:19 +0530

    User model with secure password

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Hi", email: "hi@yahoo.co.in")
+    @user = User.new(name: "Hi", email: "hi@yahoo.co.in", password: "pubbar123", password_confirmation: "pubbar123")
   end
 
   test "should be valid" do
@@ -16,7 +16,32 @@ class UserTest < ActiveSupport::TestCase
 
   test "email should be present" do
     @user.email = " "
-    assert !(@user.email?)
+    assert !(@user.valid?)
+  end
+
+  test "email should not be too long" do
+    @user.email = "a"*244 + "@example.com"
+    assert !(@user.valid?)
+  end
+
+  test "name should not be long" do
+  @user.name = "a" * 51
+  assert !(@user.valid?)
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert !(@user.valid?), "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be unique" do
+    dup_user = @user.clone
+    @user.save
+    assert(!(dup_user.valid?))
   end
   # test "the truth" do
   #   assert true
